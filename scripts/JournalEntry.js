8a4dfe7a9e2fe7cf71ad091374c423ec30289912
blusanders <blusanders@yahoo.com>
2021-01-15 11:00:51 -0600

    Stopping Point. HTML ok. JS ok.

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -4,15 +4,16 @@
  */
 export const JournalEntryComponent = (entry) => {
     return `
-       <div class="displayJournalEntry">
-         <p class=displayJournalEntry__date>${entry.date}</p>
-         <p class=displayJournalEntry__concept>${entry.concept}</p>
-         <p class=displayJournalEntry__mood>${entry.mood}</p>
-         <p class=displayJournalEntry__entry>${entry.entry}</p>
+         <div class="displayJournalEntry">
+         <p class="displayJournalEntry__concept displayJournalEntry__p">${entry.concept}</p>
+         <p class="displayJournalEntry__date displayJournalEntry__p">${entry.date}</p>
+         <p class="displayJournalEntry__mood displayJournalEntry__p">${entry.mood}</p>
+         <p class="displayJournalEntry__entry ">${entry.entry}</p>
          <p>
          <input type="button" id="journalFields__button" value="Edit">
          <input type="button" id="journalFields__button" value="Delete">
          </p>
+         <hr>
        </div>
 
     `
