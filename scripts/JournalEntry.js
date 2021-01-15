2fea0bf2aedbe33a7b623f81c038cddb8277deab
blusanders <blusanders@yahoo.com>
2021-01-15 10:19:06 -0600

    subtle formatting changes

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -4,11 +4,15 @@
  */
 export const JournalEntryComponent = (entry) => {
     return `
-       <div class=displayJournalEntry class="journalEntry">
+       <div class="displayJournalEntry">
          <p class=displayJournalEntry__date>${entry.date}</p>
          <p class=displayJournalEntry__concept>${entry.concept}</p>
          <p class=displayJournalEntry__mood>${entry.mood}</p>
          <p class=displayJournalEntry__entry>${entry.entry}</p>
+         <p>
+         <input type="button" id="journalFields__button" value="Edit">
+         <input type="button" id="journalFields__button" value="Delete">
+         </p>
        </div>
 
     `
