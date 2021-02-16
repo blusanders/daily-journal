cf89aaebdf760f182e8d455955d0d3a0adb8b57c
blusanders <blusanders@yahoo.com>
2021-02-16 08:18:53 -0600

    Added ALL sort radio button. Now change list per selection

--- scripts/filter/MoodFilter.js
+++ scripts/filter/MoodFilter.js
@@ -3,6 +3,8 @@ export const MoodFilter = (allMoods) => {
     return `
         <fieldset class="fieldset">
             <legend>Filter Journal Entries by Mood</legend>
+            <input checked type="radio" name="moodFilter" value="0"/>
+            <label for="moodFilter--all">All</label>
             ${
                 allMoods.map(
                     (mood) => {
